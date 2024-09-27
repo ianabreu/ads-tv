753d99d14823c69956def358520d24e8a66ab4b0
Ian Abreu <ianabreudev@gmail.com>
2024-09-27 05:49:49 -0300

    implement NewAlbum to gallery page

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-import { Container, SearchAndFilterRow, Title } from "@/components";
+import { Container, Modal, Title } from "@/components";
 import { useApi } from "@/hooks/useApi";
 import { AlbumItem } from "@/components/item-album";
 import { Grid } from "@/components/grid";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { NewAlbum } from "@/components/new-album";
 
 export default function Gallery() {
   const { albums, deleteAlbum, loadingAlbums } = useApi();
   const [loadImages, setLoadImages] = useState<string[]>([]);
+
+  const [openNewAlbum, setOpenNewAlbum] = useState<boolean>(false);
+
+  function handleCloseModal() {
+    setOpenNewAlbum(false);
+  }
+
   function handleImageLoad(id: string) {
     setLoadImages((prevImageloaded) => [...prevImageloaded, id]);
   }
@@ -14,7 +24,22 @@ export default function Gallery() {
   return (
     <Container>
       <Title>Galeria</Title>
-      <SearchAndFilterRow filterOptions={["ian"]} onCreate={() => {}} />
+
+      <div className="my-2 text-right">
+        <Button
+          variant={"default"}
+          size={"sm"}
+          className="w-full md:max-w-40"
+          onClick={() => {
+            setOpenNewAlbum(true);
+          }}
+        >
+          {<Plus />} Novo Álbum
+        </Button>
+      </div>
+      <hr className="border-slate-700" />
+
+      {/*--------------------------------------- Exibir lista ----------------------*/}
       <Grid
         loading={loadingAlbums}
         isEmpty={albums.length < 1}
@@ -22,6 +47,7 @@ export default function Gallery() {
       >
         {albums.map((album) => (
           <AlbumItem
+            key={album.id}
             album={album}
             deleteAlbum={deleteAlbum}
             loadedImages={loadImages}
@@ -29,6 +55,13 @@ export default function Gallery() {
           />
         ))}
       </Grid>
+      <Modal
+        isOpen={openNewAlbum}
+        onClose={handleCloseModal}
+        header="Novo álbum"
+      >
+        <NewAlbum handleCloseModal={handleCloseModal} />
+      </Modal>
     </Container>
   );
 }
