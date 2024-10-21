cac10c44cf6ec711dac9f56f27865b2d1050f8fd
Ian Abreu <ianabreudev@gmail.com>
2024-10-21 06:10:05 -0300

    add confirm and xs style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed",
   {
     variants: {
       variant: {
         default: "bg-amber-500 text-primary hover:bg-amber-500/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        confirm:
+          "bg-green-700 text-destructive-foreground hover:bg-green-700/80",
+        alert: "bg-amber-700 text-destructive-foreground hover:bg-amber-700/80",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-slate-400 text-slate-400 bg-transparent hover:bg-slate-400 hover:border-slate-400 hover:text-slate-900",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -22,6 +25,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
+        xs: "h-7 rounded-md px-2",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
