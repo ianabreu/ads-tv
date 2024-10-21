5b1c3b7550b8c1065bb644d48625f49ecb565797
Ian Abreu <ianabreudev@gmail.com>
2024-10-21 06:18:04 -0300

    list ads in anuncios page

--- src/pages/anuncios.tsx
+++ src/pages/anuncios.tsx
@@ -1,56 +1,110 @@
-import { Container, Title } from "@/components";
+import { Container, Modal, Title } from "@/components";
 import { Grid } from "@/components/grid";
-import { Ads } from "@/types";
+import { NewAds } from "@/components/new-ads";
+import { Button } from "@/components/ui/button";
+import { useApi } from "@/hooks/useApi";
+import { cn } from "@/lib/utils";
+import { Edit, Play, Plus } from "lucide-react";
 
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Anuncios() {
-  const [anuncios] = useState<Ads[]>([]);
-  const [loading] = useState(false);
+  const { ads, loadingAds } = useApi();
+  const [openNewAds, setOpenNewAds] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {}, []);
 
   const [loadImages, setLoadImages] = useState<string[]>([]);
   function handleImageLoad(id: string) {
     setLoadImages((prevImageloaded) => [...prevImageloaded, id]);
   }
-
+  function goTo(slug: string, play: boolean = false) {
+    if (play) {
+      navigate(`/anuncio/${slug}`);
+    } else {
+      navigate(`/editar/anuncio/${slug}`);
+    }
+  }
   return (
     <Container>
       <Title>Meus Anúncios</Title>
-      <Grid loading={loading} isEmpty={anuncios.length < 1} emptyType="anuncio">
-        {anuncios.map((anuncio) => (
-          <Link to={`/albuns/${anuncio.slug}`} key={anuncio.id}>
-            <section className="w-full rounded-lg p-2 bg-slate-700">
-              <div
-                className="w-full rounded-lg h-32 aspect-square bg-slate-300"
-                style={{
-                  display: loadImages.includes(anuncio.id) ? "none" : "block",
-                }}
-              ></div>
+      <div className="my-2 text-right">
+        <Button
+          variant={"default"}
+          size={"sm"}
+          className="w-full md:max-w-40"
+          onClick={() => {
+            setOpenNewAds(true);
+          }}
+        >
+          {<Plus />} Novo Anúncio
+        </Button>
+      </div>
+      <hr className="border-slate-700" />
 
-              <div
-                className="w-full rounded-lg mb-2 max-h-32 aspect-square transition-all overflow-hidden"
-                style={{
-                  display: loadImages.includes(anuncio.id) ? "block" : "none",
+      <Grid loading={loadingAds} isEmpty={ads.length < 1} emptyType="anuncio">
+        {ads.map((anuncio) => (
+          <section
+            key={anuncio.id}
+            className={cn(
+              "w-full rounded-lg p-2 bg-slate-700 border-green-700 border-l-8",
+              { "border-amber-700 ": anuncio.draft }
+            )}
+          >
+            <div
+              className="w-full max-w-36 rounded-lg mb-2 transition-all overflow-hidden m-auto"
+              style={{
+                display: loadImages.includes(anuncio.id) ? "block" : "none",
+              }}
+            >
+              <img
+                src={"./images/film.svg"}
+                alt={anuncio.title}
+                className="h-full w-full object-cover"
+                onLoad={() => {
+                  handleImageLoad(anuncio.id);
                 }}
-              >
-                <img
-                  src={anuncio.cover || "./images/folder.png"}
-                  alt={anuncio.title}
-                  className="h-full w-full object-cover"
-                  onLoad={() => {
-                    handleImageLoad(anuncio.id);
-                  }}
-                />
-              </div>
-
-              <span className="text-sm mt-2 text-background text-ellipsis line-clamp-2">
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-sm text-background text-ellipsis line-clamp-2">
                 {anuncio.title}
-              </span>
-            </section>
-          </Link>
+              </p>
+              <p className="text-xs text-background text-ellipsis">
+                <span className="font-bold">Status:</span>{" "}
+                {anuncio.draft ? "Rascunho" : "Publicado"}
+              </p>
+              <div className="flex gap-1 w-full justify-between">
+                <Button
+                  variant={"alert"}
+                  size={"xs"}
+                  onClick={() => goTo(anuncio.slug)}
+                >
+                  <Edit size={16} /> Editar
+                </Button>
+                <Button
+                  variant={"confirm"}
+                  size={"xs"}
+                  disabled={anuncio.draft}
+                  className="select-none"
+                  onClick={() => goTo(anuncio.slug, true)}
+                >
+                  <Play size={16} /> Assistir
+                </Button>
+              </div>
+            </div>
+          </section>
         ))}
       </Grid>
+      <Modal
+        isOpen={openNewAds}
+        onClose={() => setOpenNewAds(false)}
+        header="Novo Anúncio"
+      >
+        <NewAds handleCloseModal={() => setOpenNewAds(false)} />
+      </Modal>
     </Container>
   );
 }
